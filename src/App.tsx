bee8f74a134694a0f8f609a66456957ec56db70d
Antoine COULON <a.antoinecoulon@gmail.com>
2025-05-02 18:40:48 +0200

    Add: firebase not complete config

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useRef } from 'react'
+import { initializeApp } from 'firebase/app'
 import './App.css'
 
 {/** 
@@ -9,6 +10,19 @@ import './App.css'
   - extension chrome (manifest)
   - firebase  
 */}
+const API_KEY = import.meta.env.VITE_FIREBASE_API_KEY
+const firebaseConfig = {
+
+  apiKey: API_KEY,
+  authDomain: "leads-tracker-app-f3167.firebaseapp.com",
+  databaseURL: "https://leads-tracker-app-f3167-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "leads-tracker-app-f3167",
+  storageBucket: "leads-tracker-app-f3167.firebasestorage.app",
+  messagingSenderId: "790813084858",
+  appId: "1:790813084858:web:60f50cb888a46591600683"
+};
+const app = initializeApp(firebaseConfig)
+
 
 function App() {
   const [savedList, setSavedList] = useState(["test", "test"])
@@ -38,8 +52,8 @@ function App() {
         </div>
         <div className="saved-list">
           <ul>
-            {savedList.map(saved => (
-              <li>{saved}</li>              
+            {savedList.map((saved, idx) => (
+              <li key={idx}>{saved}</li>              
             ))}
           </ul>
         </div>
