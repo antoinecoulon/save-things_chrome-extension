fa0ef0677238fec64ce6dbf1b0f50298c23f3024
Antoine COULON <a.antoinecoulon@gmail.com>
2025-05-02 16:44:33 +0200

    Update:  save+erase logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
+import { useState, useRef } from 'react'
 import './App.css'
 
 {/** 
   TODO:
-    - accessibility
-    - design
-    - extension chrome (manifest)
-    - firebase  
+  - logic
+  - accessibility
+  - design
+  - extension chrome (manifest)
+  - firebase  
 */}
 
 function App() {
-  
+  const [savedList, setSavedList] = useState(["test", "test"])
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  function saveValue() {
+    if (inputRef.current?.value != null && inputRef.current?.value != "") {
+      const inputValue = inputRef.current?.value
+      setSavedList([...savedList, inputValue])
+      inputRef.current.value = ""
+    }
+  }
+
+  function eraseAll() {
+    setSavedList([])
+  }
 
   return (
     <section className='app-body'>
         <label htmlFor='app-input'>
           Entrer un URL ou une note à sauvegarder :
         </label>
-        <input type="text" className='app-input' id='app-input' name='app-input'/>
+        <input ref={inputRef} type="text" className='app-input' id='app-input' name='app-input'/>
         <div className="buttons">
-          <button>SAUVEGARDER</button>
-          <button>EFFACER</button>
+          <button onClick={saveValue}>SAUVEGARDER</button>
+          <button onClick={eraseAll}>TOUT EFFACER</button>
+        </div>
+        <div className="saved-list">
+          <ul>
+            {savedList.map(saved => (
+              <li>{saved}</li>              
+            ))}
+          </ul>
         </div>
     </section>
   )
